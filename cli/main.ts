14fe3afbd22620db911a3e7ce05b990d84e1ec94
aaron.pritzlaff <aaron@kindservices.co.uk>
2024-11-11 10:45:31 +0000

    added picocolors and emojis

--- cli/main.ts
+++ cli/main.ts
@@ -1,8 +1,18 @@
+import * as emoji from "npm:node-emoji";
+import pc from "npm:picocolors";
+
+
 export function add(a: number, b: number): number {
   return a + b;
 }
 
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
+  console.log(emoji.emojify(`:sauropod: :heart:  npm`));
+
+  console.log(
+    pc.green(`How are ${pc.italic(`you`)} doing?`)
+  )
+
   console.log("Add 2 + 3 =", add(2, 3));
 }
