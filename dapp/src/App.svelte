609af267f54e006873a5f2b6116eaa720c89d6f5
aaron.pritzlaff <aaron@kindservices.co.uk>
2024-11-11 21:15:00 +0000

    Split out list accounts

--- dapp/src/App.svelte
+++ dapp/src/App.svelte
@@ -1,68 +1,13 @@
 <script lang="ts">
-  import { onMount } from 'svelte'
-  import { ethers } from 'ethers'
-
-
-  let accounts = []
-  let errorMessage = ''
-  let infoMessage = ''
-
-  // Function to fetch accounts from MetaMask
-  async function getAccounts() {
-      if (typeof window.ethereum !== 'undefined') {
-          try {
-              // Request MetaMask accounts
-              await window.ethereum.request({ method: 'eth_requestAccounts' });
-
-              // Create an ethers provider
-              const provider = new ethers.BrowserProvider(window.ethereum);
-
-              // Get the list of accounts
-              const accountList = await provider.listAccounts();
-              infoMessage = infoMessage + '; listing accounts'
-              accounts = accountList.map(account => account.address);
-              errorMessage = '';
-          } catch (error) {
-              errorMessage = 'Error fetching accounts: ' + error.message;
-          }
-      } else {
-          errorMessage = 'MetaMask is not installed. Please install it to continue.';
-      }
-  }
-
-  // Fetch accounts on component mount (optional)
-  onMount(() => {
-      getAccounts();
-  });
+    import { Button, Modal } from 'svelteux';
+    let showModal = false;
 </script>
 
-<main>
-  <h1>MetaMask Accounts</h1>
-  <button on:click={getAccounts}>List Accounts</button>
+<Button on:click={() => (showModal = true)}>Open Modal</Button>
 
-  {#if errorMessage}
-      <p style="color: red;">{errorMessage}</p>
-  {/if}
-
-  {#if infoMessage}
-    <p style="color: blue;">{infoMessage}</p>
-  {/if}
-
-  <ul>
-      {#each accounts as account}
-          <li>{account}</li>
-      {/each}
-  </ul>
-</main>
-
-<style>
-  main {
-      text-align: center;
-      margin-top: 2rem;
-  }
-  button {
-      margin-top: 1rem;
-      padding: 0.5rem 1rem;
-      cursor: pointer;
-  }
-</style>
+{#if showModal}
+<Modal on:close={() => (showModal = false)}>
+    <h1>Hello from SvelteUX Modal!</h1>
+    <Button on:click={() => (showModal = false)}>Close</Button>
+</Modal>
+{/if}
---
+++ dapp/src/ListAccounts.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+  import { onMount } from 'svelte'
+  import { ethers } from 'ethers'
+
+
+  let accounts = []
+  let errorMessage = ''
+  let infoMessage = ''
+
+  // Function to fetch accounts from MetaMask
+  async function getAccounts() {
+      if (typeof window.ethereum !== 'undefined') {
+          try {
+              // Request MetaMask accounts
+              await window.ethereum.request({ method: 'eth_requestAccounts' });
+
+              // Create an ethers provider
+              const provider = new ethers.BrowserProvider(window.ethereum);
+
+              // Get the list of accounts
+              const accountList = await provider.listAccounts();
+              infoMessage = infoMessage + '; listing accounts'
+              accounts = accountList.map(account => account.address);
+              errorMessage = '';
+          } catch (error) {
+              errorMessage = 'Error fetching accounts: ' + error.message;
+          }
+      } else {
+          errorMessage = 'MetaMask is not installed. Please install it to continue.';
+      }
+  }
+
+  // Fetch accounts on component mount (optional)
+  onMount(() => {
+      getAccounts();
+  });
+</script>
+
+<main>
+  <h1>MetaMask Accounts</h1>
+  <button on:click={getAccounts}>List Accounts</button>
+
+  {#if errorMessage}
+      <p style="color: red;">{errorMessage}</p>
+  {/if}
+
+  {#if infoMessage}
+    <p style="color: blue;">{infoMessage}</p>
+  {/if}
+
+  <ul>
+      {#each accounts as account}
+          <li>{account}</li>
+      {/each}
+  </ul>
+</main>
+
+<style>
+  main {
+      text-align: center;
+      margin-top: 2rem;
+  }
+  button {
+      margin-top: 1rem;
+      padding: 0.5rem 1rem;
+      cursor: pointer;
+  }
+</style>
